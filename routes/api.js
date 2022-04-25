08a5c476e1e938f02116cd29cb1a1813c1f47176
test <test>
2022-04-25 19:07:26 +0700

    done

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
-/* GET home page. */
+var path = require('path');
 
 router.get('/companies', (req, res) => {
   const companies = require('./../storage/companies.json')
@@ -9,10 +9,19 @@ router.get('/companies', (req, res) => {
   res.json(companies)
 })
 
+router.get('/companies/:id/image', (req, res) => {
+  const companies = require('./../storage/companies.json')
+  const company = companies.find(comp => comp.id === req.params.id)
+
+  if (company) {
+    let imgPath = __dirname + './../public/images/' + company.logo
+    res.sendFile(path.resolve(imgPath) )
+  } else res.sendStatus(400)
+})
+
 
 router.get('/tickets', (req, res) => {
   let tickets = require('./../storage/tickets.json')
-  console.log(req.query)
 
   if (tickets && tickets.length) {
     const segments = require('./../storage/segments.json')
@@ -22,28 +31,46 @@ router.get('/tickets', (req, res) => {
         ticket.segments = ticket.segments.map(segment => {
           return segments.find(item => item.id === segment) || segment
         })
+        ticket.stops = ticket.segments.length
+        ticket.duration = 0 
+        for (let segment of ticket.segments) {
+          ticket.duration += segment.duration
+          ticket.stops += segment.stops.length
+        }
         return ticket
       }
     })
   }
 
+  if ('filters' in req.query) {
+    const companies = require('./../storage/companies.json')
+    let filters = JSON.parse(req.query.filters)
+
+    if (filters.company) {
+      tickets = tickets.filter(ticket => {
+        let company = companies.find(c => c.id === ticket.companyId)
+        return company.name === filters.company ? company : false
+      })
+    }
+    if (filters.transferCounts && filters.transferCounts.length) {
+      console.dir(filters.transferCounts)
+      tickets = tickets.filter(ticket => {
+        let stops = ticket.stops - ticket.segments.length 
+        return filters.transferCounts.indexOf(stops.toString()) !== -1 ? ticket : false
+      })
+    }
+  }
+
   if ('sort' in req.query) {
     switch (req.query.sort) {
       case 'Cheapest': tickets = _.sortBy(tickets, 'price'); break;
-      case 'Fastest': tickets = _.sortBy(tickets, ticket => {
-        return ticket.segments.reduce((prevSeg, currSeg) => currSeg ? currSeg.duration + prevSeg.duration : 0)
-      }); break; 
-      case 'Optimal': tickets = tickets.sort((a, b) => {
-        a.price < b.price ? 1 : -1
-      }).sort((a, b) => {
-        let aDuration = a.segments.reduce((prevSeg, currSeg) => currSeg + prevSeg)
-        let bDuration = b.segments.reduce((prevSeg, currSeg) => currSeg + prevSeg)
-        aDuration < bDuration ? 1 : -1
-      }); break; 
+      case 'Fastest': tickets = _.sortBy(tickets, 'duration', 'price'); break; 
+      case 'Optimal': tickets = _.sortBy(tickets, ['stops', 'price', 'duration']); break; 
     }
   }
   if ('limit' in req.query) {
-    tickets = tickets.slice(0, parseInt(req.query.limit))
+    let skip = parseInt(req.query.skip || 0)
+    tickets = tickets.slice(skip, skip + parseInt(req.query.limit))
   }
 
 
